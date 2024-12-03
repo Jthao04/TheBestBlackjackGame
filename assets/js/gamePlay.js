06d2dcfc390a289225a9f83da1c990d46716aad7
scheeseb <samuel.cheeseboro@gmail.com>
2024-12-03 12:25:12 -0600

    Made the script run

--- assets/js/gamePlay.js
+++ assets/js/gamePlay.js
@@ -93,37 +93,6 @@ function dealersTurn() {
 function playersTurn() {
 
 
-// Attach event listener to hit button element
-hitEl.addEventListener('click', function () {
-    card = drawCard();
-    player.cards.push(card);
-    addCardToScore(player);
-    if(player.score>=21){
-        if(player.score==21){
-            dealersTurn();
-        }
-        else{
-            showModal("You Busted! You Lose!");
-        }
-    }
-});
-
-// Attach event listener to decrement button element
-stayEl.addEventListener('click', function () {
-    dealersTurn()
-    if(dealer.score>=22){
-        showModal("The Dealer Busted! You Win!");
-    }else{ 
-        if(player.score>dealer.score){
-            showModal("You Win!!");
-        }else if(player.score<dealer.score){
-            showModal("You Lose");
-        }else{
-            showModal("You Pushed With The Dealer")
-
-    const hitEl = document.querySelector('#hit');
-    const stayEl = document.querySelector('#stay');
-
     // Attach event listener to hit button element
     hitEl.addEventListener('click', function () {
         card = drawCard();
@@ -136,27 +105,62 @@ stayEl.addEventListener('click', function () {
             else {
                 showModal("You Busted! You Lose!");
             }
-
         }
     });
-
-    // Attach event listener to decrement button element
-    stayEl.addEventListener('click', function () {
-        dealersTurn()
-        if (dealer.score >= 22) {
-            showModal("The Dealer Busted! You Win!");
-        } else {
-            if (player.score > dealer.score) {
-                showModal("You Win!!");
-            } else if (player.score < dealer.score) {
-                showModal("You Lose");
-            } else {
-                showModal("You Pushed With The Dealer")
-            }
-        }
-    });
-
 }
+
+// // Attach event listener to decrement button element
+// stayEl.addEventListener('click', function () {
+//     dealersTurn()
+//     if (dealer.score >= 22) {
+//         showModal("The Dealer Busted! You Win!");
+//     } else {
+//         if (player.score > dealer.score) {
+//             showModal("You Win!!");
+//         } else if (player.score < dealer.score) {
+//             showModal("You Lose");
+//         } else {
+//             showModal("You Pushed With The Dealer")
+//         }
+//     }
+// })
+const hitEl = document.querySelector('#hit');
+const stayEl = document.querySelector('#stay');
+
+// Attach event listener to hit button element
+hitEl.addEventListener('click', function () {
+    card = drawCard();
+    player.cards.push(card);
+    addCardToScore(player);
+    if (player.score >= 21) {
+        if (player.score == 21) {
+            dealersTurn();
+        }
+        else {
+            showModal("You Busted! You Lose!");
+        }
+
+    }
+})
+
+
+// Attach event listener to decrement button element
+stayEl.addEventListener('click', function () {
+    dealersTurn()
+    if (dealer.score >= 22) {
+        showModal("The Dealer Busted! You Win!");
+    } else {
+        if (player.score > dealer.score) {
+            showModal("You Win!!");
+        } else if (player.score < dealer.score) {
+            showModal("You Lose");
+        } else {
+            showModal("You Pushed With The Dealer")
+        }
+    }
+});
+
+
 
 
 function scoreBeginningDeal(gameOver) {
